5dcb2bfe7c17d5879fdab3555b5470d57b80e3d6
chungmcl <chungmcl@cs.washington.edu>
2022-03-03 15:47:53 -0800

    interim

--- syscall.c
+++ syscall.c
@@ -84,8 +84,12 @@ uintptr_t dispatch_edgecall_ocall( unsigned long call_id,
 
   timing_buff_push(&edge_call->call_id, &call_id, sizeof(call_id));
   //timing_buff_remove();
-  print_strace("[runtime] time: %lu \r\n", sbi_get_time());
-  print_strace("[runtime] interval: %lu\r\n", sbi_get_interval_len());
+  //print_strace("[runtime] time: %lu \r\n", sbi_get_time());
+  //print_strace("[runtime] interval: %lu\r\n", sbi_get_interval_len());
+
+  // DEBUGGING: hijack data and data_len to print out debug stuff
+  data_len += 1;
+  ((char*)data)[data_len - 1] = "2";
 
   // chungmcl
   uintptr_t buffer_data_start = edge_call_data_ptr();
