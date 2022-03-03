90d7f8560a933f461f0314aefdc7a24be280797d
chungmcl <chungmcl@cs.washington.edu>
2022-03-02 21:17:51 -0800

    interim

--- syscall.c
+++ syscall.c
@@ -82,7 +82,7 @@ uintptr_t dispatch_edgecall_ocall( unsigned long call_id,
   // chungmcl
   //edge_call->call_id = call_id;
 
-  timing_buff_push(edge_call->call_id, call_id, sizeof(call_id));
+  timing_buff_push(&edge_call->call_id, &call_id, sizeof(call_id));
   timing_buff_remove();
 
   // chungmcl
