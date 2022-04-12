86884d91210c091519ebb0fbe4403dc91f90837b
chungmcl <chungmcl@cs.washington.edu>
2022-04-12 07:59:22 -0700

    interim

--- syscall.c
+++ syscall.c
@@ -100,6 +100,7 @@ uintptr_t dispatch_edgecall_ocall( unsigned long call_id,
   debug_timing_buff();
   int count = timing_buff_get_count();
   print_strace("get_count(): %i\n", count);
+  timing_buff_remove();
   //for (int i = 0; i < 200; i++) {
   //  sbi_pause();
   //}
