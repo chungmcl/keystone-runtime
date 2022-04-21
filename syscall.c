e9fac1cebafb23c4eeaf3ff52faf54b448e24db0
chungmcl <chungmcl@cs.washington.edu>
2022-04-20 22:12:16 -0700

    interim

--- syscall.c
+++ syscall.c
@@ -79,13 +79,13 @@ uintptr_t dispatch_edgecall_ocall( unsigned long call_id,
    * region, calculate the offsets to the argument data, and then
    * dispatch the ocall to host */
 
-  edge_call->call_id = call_id;
+  // edge_call->call_id = call_id;
 
-  // /** chungmcl **/
-  // timing_buff_push((void*)&edge_call->call_id, &call_id, sizeof(call_id));
-  // if (!timing_buff_remove()) {
-  //   print_strace("edge_call->call_id removal failed\n");
-  // }
+  /** chungmcl **/
+  timing_buff_push((void*)&edge_call->call_id, &call_id, sizeof(call_id));
+  if (!timing_buff_remove()) {
+    print_strace("edge_call->call_id removal failed\n");
+  }
 // 
   // unsigned int a = 18;
   // timing_buff_push(&edge_call->call_id, &a, sizeof(a));
