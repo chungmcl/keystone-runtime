3ea014433690e0f4dada7ef79e5fada553b5abd5
chungmcl <chungmcl@cs.washington.edu>
2022-11-24 15:54:24 -0800

    interim

--- syscall.c
+++ syscall.c
@@ -149,6 +149,7 @@ uintptr_t handle_copy_from_shared(void* dst, uintptr_t offset, size_t size){
 }
 
 void handle_print_time() {
+  print_strace("handle_print_time start\n");
   uint64_t LOOPS = 100000000000000;
   
   int array[15000];
