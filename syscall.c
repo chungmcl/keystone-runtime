1707d51795961662a428c62b9e612b7271dd6dad
chungmcl <chungmcl@cs.washington.edu>
2022-11-24 16:39:27 -0800

    interim

--- syscall.c
+++ syscall.c
@@ -150,7 +150,7 @@ uintptr_t handle_copy_from_shared(void* dst, uintptr_t offset, size_t size){
 
 void handle_print_time() {
   print_strace("handle_print_time start\n");
-  const int LOOPS = 8000;
+  const int LOOPS = 100000;
   
   int array[LOOPS];
   int i = 0;
