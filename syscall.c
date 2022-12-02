8dd65e8c4d19b0a87d03df19c56d74bc1ac3ad74
chungmcl <chungmcl@cs.washington.edu>
2022-12-01 18:25:17 -0800

    interim

--- syscall.c
+++ syscall.c
@@ -281,6 +281,9 @@ void handle_syscall(struct encl_ctx* ctx)
   uintptr_t arg3 = ctx->regs.a3;
   uintptr_t arg4 = ctx->regs.a4;
 
+
+  print_strace("n: %d\n", n);
+
 #if FUZZ
   if (n != RUNTIME_SYSCALL_EXIT && n != RUNTIME_SYSCALL_OCALL) {
     fuzzy_buff_flush_due_items(sbi_get_time());
