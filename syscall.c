859cd71b8314b8ff1e4805f16e9d41a3ee69a7ae
chungmcl <chungmcl@cs.washington.edu>
2022-12-01 18:22:58 -0800

    interim

--- syscall.c
+++ syscall.c
@@ -236,6 +236,7 @@ void handle_print_time() {
 }
 
 void handle_reg_clock_ipi(uintptr_t process) {
+  print_strace("process: %p\n", process);
   sbi_reg_clock_ipi((void (*)(struct sbi_scratch *scratch))process);
 }
 
